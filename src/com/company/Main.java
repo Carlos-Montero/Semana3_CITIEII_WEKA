29a3f09613de5d64201f8c3a57ede3bd7d5e6f68
Carlos <carlosmonteroreina@gmail.com>
2018-03-04 17:14:27 +0100

    Modificación. Falta Cross Validation 10.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,4 @@
+/*
 package com.company;
 import weka.core.Instances;
 import java.io.BufferedReader;
@@ -21,21 +22,68 @@ public class Main {
         if (data.classIndex() == -1)
             data.setClassIndex(data.numAttributes() - 1);
 
+    }
+}
+*/
+
+//package wekaexamples.classifiers;
+
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.core.converters.ConverterUtils.DataSource;
+
+/**
+ * This example class trains a J48 classifier on a dataset and outputs for
+ * a second dataset the actual and predicted class label, as well as the
+ * class distribution.
+ */
+public class OutputClassDistribution {
+
+    /**
+     * Expects two parameters: training file and test file.
+     *
+     * @param args	the commandline arguments
+     * @throws Exception	if something goes wrong
+     */
+    public static void main(String[] args) throws Exception {
+        // load data
 
 
+        //obtenemos el diabetes.arff del directorio en el que está      ESTO NO ES DEL CODIGO ORIGINAL
+        DataSource source = new DataSource("/Users/Carlos/Desktop/weka-3-8-1/data/diabetes.arff");
 
 
+        Instances train = DataSource.read(args[0]);
+        train.setClassIndex(train.numAttributes() - 1);
+        Instances test = DataSource.read(args[1]);
+        test.setClassIndex(test.numAttributes() - 1);
+        if (!train.equalHeaders(test))
+            throw new IllegalArgumentException(
+                    "Train and test set are not compatible: " + train.equalHeadersMsg(test));
 
+        // train classifier
+        J48 cls = new J48();
+        cls.buildClassifier(train);
 
-
-
-
-
-
-
-
-
-
-
+        // output predictions
+        System.out.println("# - actual - predicted - error - distribution");
+        for (int i = 0; i < test.numInstances(); i++) {
+            double pred = cls.classifyInstance(test.instance(i));
+            double[] dist = cls.distributionForInstance(test.instance(i));
+            System.out.print((i+1));
+            System.out.print(" - ");
+            System.out.print(test.instance(i).toString(test.classIndex()));
+            System.out.print(" - ");
+            System.out.print(test.classAttribute().value((int) pred));
+            System.out.print(" - ");
+            if (pred != test.instance(i).classValue())
+                System.out.print("yes");
+            else
+                System.out.print("no");
+            System.out.print(" - ");
+            System.out.print(Utils.arrayToString(dist));
+            System.out.println();
+        }
     }
 }
